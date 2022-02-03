ad279725f30d4a1ac8a35935d165844ce8630cb7
roykimm <roykimmyunghoon@gmail.com>
2022-02-03 17:55:43 +0900

    t

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>c 2022 Gobella</p>
+      </footer>  
+    </div>
+  )
 }
 
 export default MyApp
---
+++ pages/world.js
@@ -0,0 +1,5 @@
+const World = () =>{
+    return <div>Hello world world1</div>;
+}
+
+export default World
